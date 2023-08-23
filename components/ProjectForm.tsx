0a01f76fab734098d507bdde6436a544a4c154c7
Marcin Bieniek <marcingim@gmail.com>
2023-08-23 14:34:03 +0200

    Add complete create project form design with image upload function

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -2,8 +2,11 @@
 
 import { SessionInterface } from "@/common.types"
 import Image from "next/image"
-import { ChangeEvent } from "react"
+import { ChangeEvent, useState } from "react"
 import FormField from "./FormField"
+import { categoryFilters } from "@/constant"
+import CustomMenu from "./CustomMenu"
+import Button from "./Button"
 
 type Props = {
   type: string,
@@ -13,13 +16,42 @@ type Props = {
 const ProjectForm = ({ type, session} : Props) => {
 
   const handleFormSubmit = (e: React.FormEvent) => {};
-  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
-  const handleStateChange = (fieldName: string, value: string) => {};
 
-  const form = {
-    image: '',
-    title: ''
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const file = e.target.files?.[0]
+
+    if(!file) return;
+
+    if(!file.type.includes('image')) {
+      return alert('Please upload an image file')
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange('image', result)
+    }
   };
+
+  const handleStateChange = (fieldName: string, value: string) => {
+    setForm((prevState) => ({ ...prevState, [fieldName]: value }))
+  };
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    image: '',
+    liveSiteUrl: '',
+    githubUrl: '',
+    category: ''
+  })
 
   return (
     <form
@@ -78,17 +110,28 @@ const ProjectForm = ({ type, session} : Props) => {
         setState={(value) => handleStateChange('githubUrl', value)}
       />
 
-      <FormField
-        title="Title"
-        state={form.title}
-        placeholder="Flexibble"
-        setState={(value) => handleStateChange('title', value)}
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange('category', value)}
       />
 
-      {/* CustomInput Category... */}
-
       <div className="flexStart w-full">
-        <button>Create</button>
+
+        <Button
+          title={
+            isSubmitting
+              ? `${type === 'create'
+                ? 'Creating' : 'Editing'}`
+              : `${type === 'create'
+                ? 'Create' : 'Edit'}`
+          }
+          type="submit"
+          leftIcon={isSubmitting ? "" : '/plus.svg'}
+          isSubmitting={isSubmitting}
+        />
+
       </div>
 
     </form>
